c3d14ee39d08690f092d0a5e1d16b1aacb3641ad
avtuhovich <nadia6k@gmail.com>
2019-07-10 21:17:24 +0300

    Initial commit

---
+++ app/src/main/java/com/nadia/weather/MainActivity.java
@@ -0,0 +1,64 @@
+package com.nadia.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView cityField, detailsField, currentTemperatureField, humidity_field, pressure_field,
+            weatherIcon, updatedField;
+
+    Typeface weatherFont;
+
+    private LocationManager locationMangaer=null;
+    private LocationListener locationListener=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        weatherFont = Typeface.createFromAsset(getApplicationContext().getAssets(),
+                "fonts/weathericons-regular-webfont.ttf");
+
+        cityField = (TextView) findViewById(R.id.city_field);
+        updatedField = (TextView) findViewById(R.id.updated_field);
+        detailsField = (TextView) findViewById(R.id.details_field);
+        currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView) findViewById(R.id.humidity_field);
+        pressure_field = (TextView) findViewById(R.id.pressure_field);
+        weatherIcon = (TextView) findViewById(R.id.weather_icon);
+        weatherIcon.setTypeface(weatherFont);
+
+        Function.placeIdTask asyncTask = new Function.placeIdTask(new Function.AsyncResponse() {
+            public void processFinish(String weather_city, String weather_description,
+                                      String weather_temperature, String weather_humidity,
+                                      String weather_pressure, String weather_updateOn,
+                                      String weather_iconText, String sun_rise) {
+
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updateOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                humidity_field.setText("Humidity: " + weather_humidity);
+                pressure_field.setText("Pressure: " + weather_pressure);
+                weatherIcon.setText(Html.fromHtml(weather_iconText));
+            }
+        });
+
+        locationMangaer = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        asyncTask.execute("25.180000", "89.530000");
+    }
+}
+
+
